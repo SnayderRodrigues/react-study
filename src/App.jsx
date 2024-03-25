46ea13d6f8826de585f81a7e790bf0ba6eded90e
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-03-25 17:42:34 -0300

    installed lenis scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <Header />
