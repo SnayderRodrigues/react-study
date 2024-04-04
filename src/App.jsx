502a220f49bc8f0c64e38ea88d0a68b5d3993fb0
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-04-03 23:07:11 -0300

    add scroll to top component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import Lenis from "@studio-freight/lenis";
 import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   useEffect(() => {
@@ -19,6 +20,7 @@ function App() {
   return (
     //moved MainContent to pages/home/HomePage.jsx
     <>
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
--- src/pages/about/AboutPage.jsx
+++ src/pages/about/AboutPage.jsx
@@ -1,12 +1,38 @@
 import React from "react";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 import "./aboutpage.scss";
 
 const AboutPage = () => {
   return (
     <main>
-      <section className="aboutPage">
-        <div className="wrapper aboutPage__wrapper ">
-          <h1>AboutPage</h1>
+      <section className="aboutPageHero">
+        <div className="wrapper aboutPageHero__wrapper">
+          <h1 className="aboutPageHero__title">About Us Example Text</h1>
+        </div>
+      </section>
+      <section className="aboutPageMission">
+        <div className="wrapper aboutPageMission__wrapper">
+          <h2 className="aboutPageMission__headline">
+            <span className="highlight">Human experiences</span> are the
+            foundation of everything we do – client relationships, team
+            collaboration and an unwavering focus on the end user. This
+            philosophy is in our name, our core values and underpins our
+            approach to every engagement.
+          </h2>
+        </div>
+      </section>
+      <section className="aboutPageHistory">
+        <div className="wrapper aboutPageHistory__wrapper">
+          <h2 className="aboutPageHistory__title">Section Title</h2>
+        </div>
+      </section>
+      <section className="aboutPageCarousel">
+        <div className="aboutPageCarousel__wrapper">
+          <img src={Cat} alt="" className="image-small" />
+          <img src={Cat} alt="" className="image-medium" />
+          <img src={Cat} alt="" className="image-small" />
+          <img src={Cat} alt="" className="image-medium" />
+          <img src={Cat} alt="" className="image-small" />
         </div>
       </section>
     </main>
