3fe3132b14fc51c176227f565e915b0b6b08ab88
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-04-05 17:47:03 -0300

    creation of the low-fidelity layout of the work page

--- src/pages/work/WorkPage.jsx
+++ src/pages/work/WorkPage.jsx
@@ -1,13 +1,110 @@
 import React from "react";
 import "./workpage.scss";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 
 const WorkPage = () => {
   return (
-    <section className="workPage">
-      <div className="wrapper workPage__wrapper ">
-        <h1>WorkPage</h1>
-      </div>
-    </section>
+    <main>
+      <section className="workPage__hero">
+        <div className="wrapper workPage__wrapper">
+          <h1>
+            Work Page <br /> Example Text
+          </h1>
+        </div>
+      </section>
+      <section className="workPage__works">
+        <div className="wrapper workPage__work-wrapper">
+          <div className="workPage__works__works-wrapper">
+            <div className="workPage__works__works-full">
+              <img src={Cat} alt="" />
+              <span className="workPage__works__button button">
+                Example Name
+              </span>
+            </div>
+            <div className="workPage__works__works-half">
+              <div className="workPage__works__works-half-1">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+              <div className="workPage__works__works-half-2">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="workPage__works__works-wrapper">
+            <div className="workPage__works__works-full">
+              <img src={Cat} alt="" />
+              <span className="workPage__works__button button">
+                Example Name
+              </span>
+            </div>
+            <div className="workPage__works__works-half">
+              <div className="workPage__works__works-half-1">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+              <div className="workPage__works__works-half-2">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="workPage__works__works-wrapper">
+            <div className="workPage__works__works-full">
+              <img src={Cat} alt="" />
+              <span className="workPage__works__button button">
+                Example Name
+              </span>
+            </div>
+            <div className="workPage__works__works-half">
+              <div className="workPage__works__works-half-1">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+              <div className="workPage__works__works-half-2">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="workPage__works__works-wrapper">
+            <div className="workPage__works__works-full">
+              <img src={Cat} alt="" />
+              <span className="workPage__works__button button">
+                Example Name
+              </span>
+            </div>
+            <div className="workPage__works__works-half">
+              <div className="workPage__works__works-half-1">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+              <div className="workPage__works__works-half-2">
+                <img src={Cat} alt="" />
+                <span className="workPage__works__button button">
+                  Example Name
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
