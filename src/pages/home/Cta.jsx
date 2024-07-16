5856a97073d4d1325cbcb1f9086754b5fe4660e7
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-07-16 17:11:11 -0300

    update: cta section styles, added cards

--- src/pages/home/Cta.jsx
+++ src/pages/home/Cta.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
 import { IoMdArrowForward } from "react-icons/io";
+import { MdHideImage } from "react-icons/md";
 
 const Cta = () => {
   return (
@@ -13,15 +14,59 @@ const Cta = () => {
             atingir seus objetivos pessoais e profissionais.
           </p>
         </div>
-        <div className="cta__button-wrapper">
-          <Link to="contact" className="button cta__button link">
-            Entre em contato
-            <IoMdArrowForward />
-          </Link>
+        <div className="cta__card-wrapper">
+          <div className="cta__card">
+            <MdHideImage />
+            <span>Título</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
+              eveniet voluptatibus.
+            </span>
+            <Link to="contact" className="button cta__button link">
+              Entre em contato
+              <IoMdArrowForward />
+            </Link>
+          </div>
+          <div className="cta__card">
+            <MdHideImage />
+            <span>Título</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
+              eveniet voluptatibus.
+            </span>
+            <Link to="contact" className="button cta__button link">
+              Saiba mais sobre nós
+              <IoMdArrowForward />
+            </Link>
+          </div>
+          <div className="cta__card">
+            <MdHideImage />
+            <span>Título</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
+              eveniet voluptatibus.
+            </span>
+            <Link to="contact" className="button cta__button link">
+              Mais informações sobre os cursos
+              <IoMdArrowForward />
+            </Link>
+          </div>
+          <div className="cta__card">
+            <MdHideImage />
+            <span>Título</span>
+            <span>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
+              eveniet voluptatibus.
+            </span>
+            <Link to="contact" className="button cta__button link">
+              Leia nossas FAQs
+              <IoMdArrowForward />
+            </Link>
+          </div>
         </div>
       </div>
     </section>
   );
-}
+};
 
-export default Cta
+export default Cta;
