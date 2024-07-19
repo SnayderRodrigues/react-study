de113af0a911bc6de2f883aeba3f2e98a899cdfb
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-07-18 23:47:58 -0300

    refactor: separate expertise page components and sections into individual .jsx files

---
+++ src/pages/expertise/ExpertisePageContent.jsx
@@ -0,0 +1,147 @@
+import React from "react";
+import { MdHideImage } from "react-icons/md";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
+
+const ExpertisePageContent = () => {
+  return (
+    <section className="expertisePage-midpage">
+      <div className="wrapper expertisePage-midpage__wrapper">
+        <div className="expertisePage-midpage__image-wrapper">
+          <img src={Cat} alt="" />
+        </div>
+        <div className="expertisePage-midpage__headline">
+          <h2>Título</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod
+            mollitia dolores dolorum praesentium inventore ut quasi voluptatem
+            ea quam nam.
+          </p>
+        </div>
+        <div className="expertisePage-midpage__cards-wrapper">
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Aulas ao vivo 24h</h3>
+            <p>
+              Disponibilidade de aulas ao vivo em qualquer horário, permitindo
+              flexibilidade para os alunos.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Professores Nativos</h3>
+            <p>
+              Instrutores que são falantes nativos de inglês, garantindo uma
+              experiência autêntica e imersiva.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Conteúdo Inovador</h3>
+            <p>
+              Material didático atualizado e inovador que acompanha as
+              tendências mais recentes de ensino.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Metodologia Personalizada</h3>
+            <p>
+              Planos de estudo personalizados de acordo com as necessidades e
+              objetivos de cada aluno.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Acompanhamento Individualizado</h3>
+            <p>
+              Feedback constante e suporte individual para acompanhar o
+              progresso dos alunos.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Plataforma Interativa</h3>
+            <p>
+              Uso de uma plataforma online interativa que facilita o aprendizado
+              e o engajamento dos alunos.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Aulas de Conversação</h3>
+            <p>
+              Sessões focadas em melhorar a fluência e a confiança dos alunos ao
+              falar inglês.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Preparação para Exames</h3>
+            <p>
+              Cursos específicos para preparação de exames de proficiência, como
+              TOEFL, IELTS, entre outros.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Acompanhamento via WhatsApp</h3>
+            <p>
+              Suporte contínuo e direto pelo WhatsApp para esclarecer dúvidas e
+              oferecer orientação.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Teste de Nível</h3>
+            <p>
+              Avaliação precisa do nível de inglês dos alunos para direcionar o
+              ensino de acordo com suas necessidades específicas.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Inglês instrumental</h3>
+            <p>
+              Cursos focados em habilidades específicas, como leitura técnica ou
+              redação acadêmica, para atender às necessidades dos alunos.
+            </p>
+          </div>
+          <div className="expertisePage-midpage__card">
+            <span>
+              <MdHideImage />
+            </span>
+            <h3>Certificação Reconhecida</h3>
+            <p>
+              Certificados reconhecidos internacionalmente que comprovam o nível
+              de proficiência alcançado pelos nossos alunos.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ExpertisePageContent;
---
+++ src/pages/expertise/ExpertisePageCourses.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
+
+const ExpertisePageCourses = () => {
+  return (
+    <section className="expertisePage-ending">
+      <div className="wrapper-medium expertisePage-ending__wrapper">
+        <div className="expertisePage-ending__content">
+          <div className="expertisePage-ending__image-wrapper">
+            <img src={Cat} alt="" />
+          </div>
+          <div className="expertisePage-ending__text-wrapper">
+            <h3>Título do Curso</h3>
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error,
+              velit delectus! Dolorem esse aut pariatur debitis distinctio,
+              ipsam quidem in et est repudiandae, animi culpa deleniti itaque
+              provident perspiciatis perferendis.
+            </p>
+          </div>
+        </div>
+        <div className="expertisePage-ending__content">
+          <div className="expertisePage-ending__text-wrapper">
+            <h3>Título do Curso</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              Repellendus, nam sunt consequuntur exercitationem quos
+              consectetur! Debitis voluptate architecto unde culpa sequi odit
+              doloremque ipsam velit incidunt earum fuga, dignissimos assumenda!
+            </p>
+          </div>
+          <div className="expertisePage-ending__image-wrapper">
+            <img src={Cat} alt="" />
+          </div>
+        </div>
+        <div className="expertisePage-ending__content">
+          <div className="expertisePage-ending__image-wrapper">
+            <img src={Cat} alt="" />
+          </div>
+          <div className="expertisePage-ending__text-wrapper">
+            <h3>Título do Curso</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed enim,
+              debitis iure dignissimos expedita obcaecati iusto quidem soluta?
+              Dicta nam nisi ea nobis natus expedita consequuntur quas dolor
+              assumenda saepe.
+            </p>
+          </div>
+        </div>
+        <div className="expertisePage-ending__content">
+          <div className="expertisePage-ending__text-wrapper">
+            <h3>Título do Curso</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+              quaerat quis officiis ipsam necessitatibus. Vitae porro quibusdam
+              cupiditate voluptatem asperiores saepe quae accusantium maiores,
+              delectus accusamus nisi corporis ullam molestias.
+            </p>
+          </div>
+          <div className="expertisePage-ending__image-wrapper">
+            <img src={Cat} alt="" />
+          </div>
+        </div>
+        <div className="expertisePage-ending__content">
+          <div className="expertisePage-ending__image-wrapper">
+            <img src={Cat} alt="" />
+          </div>
+          <div className="expertisePage-ending__text-wrapper">
+            <h3>Título do Curso</h3>
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
+              iusto voluptatum fugiat eveniet ipsa vel dolore porro distinctio
+              sit! Atque placeat sapiente commodi recusandae et expedita quia,
+              quos quasi mollitia!
+            </p>
+          </div>
+        </div>
+        <div className="expertisePage-ending__content">
+          <div className="expertisePage-ending__text-wrapper">
+            <h3>Título do Curso</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
+              in provident porro! Excepturi amet aliquid vitae. Facilis,
+              consequuntur et saepe quae repellat quod qui natus cupiditate
+              incidunt! Omnis, expedita porro!
+            </p>
+          </div>
+          <div className="expertisePage-ending__image-wrapper">
+            <img src={Cat} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default ExpertisePageCourses;
