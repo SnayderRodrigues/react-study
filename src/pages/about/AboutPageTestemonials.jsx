c749ab2ac3f40505b575d094e3e46736918a7314
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-07-24 23:39:25 -0300

    update: about page testemonials section

--- src/pages/about/AboutPageTestemonials.jsx
+++ src/pages/about/AboutPageTestemonials.jsx
@@ -1,69 +1,34 @@
 import React, { useState } from "react";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 import { IoMdArrowForward } from "react-icons/io";
 import { FaAsterisk } from "react-icons/fa";
 
 const AboutPageTestemonials = () => {
-  const sliderItems = [
-    <div className="aboutPage-testemonials__slider slider-active">
-      <h2 className="aboutPage-testemonials__headline">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime vel quae
-        dicta eaque nostrum consequuntur quas officia perspiciatis? Voluptate
-        ipsum deserunt vero voluptatem blanditiis, delectus necessitatibus
-        perferendis. Suscipit, expedita corrupti?
-      </h2>
-      <div className="aboutPage-testemonials__author">
-        <div className="author-div">
-          <span className="author-name">
-            <FaAsterisk /> Nome
-          </span>
-        </div>
-      </div>
-    </div>,
-    <div className="aboutPage-testemonials__slider">
-      <h2 className="aboutPage-testemonials__headline">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
-        aperiam corrupti vel eum inventore officiis, facere praesentium quia
-        voluptatum dolores voluptates deserunt expedita maxime delectus
-        repellendus optio dolor cumque veniam?
-      </h2>
-      <div className="aboutPage-testemonials__author">
-        <div className="author-div">
-          <span className="author-name">
-            <FaAsterisk /> Nome
-          </span>
-        </div>
-      </div>
-    </div>,
-    <div className="aboutPage-testemonials__slider">
-      <h2 className="aboutPage-testemonials__headline">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat harum
-        nam aperiam minus impedit itaque voluptatibus? Magni modi blanditiis
-        possimus laborum praesentium recusandae. Nostrum facilis obcaecati
-        impedit, minima tempora natus!
-      </h2>
-      <div className="aboutPage-testemonials__author">
-        <div className="author-div">
-          <span className="author-name">
-            <FaAsterisk /> Nome
-          </span>
-        </div>
-      </div>
-    </div>,
-    <div className="aboutPage-testemonials__slider">
-      <h2 className="aboutPage-testemonials__headline">
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio officia
-        eos odio voluptas, blanditiis expedita, delectus a dolorum placeat
-        voluptates vitae. Quasi est unde ut, dolore nihil quis reiciendis
-        molestias.
-      </h2>
-      <div className="aboutPage-testemonials__author">
-        <div className="author-div">
-          <span className="author-name">
-            <FaAsterisk /> Nome
-          </span>
-        </div>
-      </div>
-    </div>,
+  const testemonialsItems = [
+    {
+      number: "01.",
+      testemonial:
+        "A Talkative transformou minha experiência de aprendizado. As aulas são dinâmicas e os professores são extremamente dedicados. Hoje, me sinto muito mais confiante para conversar em inglês!",
+      author: "Ana Paula",
+    },
+    {
+      number: "02.",
+      testemonial:
+        "A flexibilidade de horários e a qualidade dos professores da Talkative são incríveis. Consigo estudar no meu próprio ritmo, o que tem sido essencial para o meu desenvolvimento. Melhor decisão que já tomei!",
+      author: "Lucas",
+    },
+    {
+      number: "03.",
+      testemonial:
+        "A metodologia da Talkative é inovadora e envolvente. Sinto que meu inglês evoluiu muito mais rápido do que em outros cursos que já fiz. As aulas online são práticas e adaptadas às minhas necessidades. Estou muito satisfeito com os resultados!",
+      author: "Ricardo",
+    },
+    {
+      number: "04.",
+      testemonial:
+        "Graças à Talkative, consegui uma promoção no trabalho que exigia fluência em inglês. As aulas são práticas e focadas em situações reais do dia a dia. Estou muito satisfeita com meu progresso!",
+      author: "Mariana",
+    },
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -74,28 +39,42 @@ const AboutPageTestemonials = () => {
 
   const prevItem = () => {
     const newIndex =
-      currentIndex === 0 ? sliderItems.length - 1 : currentIndex - 1;
+      currentIndex === 0 ? testemonialsItems.length - 1 : currentIndex - 1;
     showItem(newIndex);
   };
 
   const nextItem = () => {
     const newIndex =
-      currentIndex === sliderItems.length - 1 ? 0 : currentIndex + 1;
+      currentIndex === testemonialsItems.length - 1 ? 0 : currentIndex + 1;
     showItem(newIndex);
   };
 
   return (
     <section className="aboutPage-testemonials">
-      <div className="wrapper-medium aboutPage-testemonials__wrapper">
-        {sliderItems.map((item, index) =>
-          React.cloneElement(item, {
-            className:
+      <div className="wrapper aboutPage-testemonials__wrapper">
+        {testemonialsItems.map((item, index) => (
+          <div
+            className={
               index === currentIndex
-                ? "aboutPage-testemonials__slider slider-active"
-                : "aboutPage-testemonials__slider",
-            key: index,
-          })
-        )}
+                ? "aboutPage-testemonials__card card-active"
+                : "aboutPage-testemonials__card"
+            }
+            key={index}
+          >
+            <div className="aboutPage-testemonials__card-content">
+              <span>{item.number}</span>
+              <h2 className="aboutPage-testemonials__headline">
+                {item.testemonial}
+              </h2>
+              <img src={Cat} alt="" />
+              <div className="aboutPage-testemonials__author">
+                <span className="author-name">
+                  <FaAsterisk /> {item.author}
+                </span>
+              </div>
+            </div>
+          </div>
+        ))}
         <div className="navigation">
           <div className="navigation-div">
             <button
