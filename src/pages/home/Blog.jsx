56552a4b0221c7e4d70752a632943f59b4468f50
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-07-28 00:24:42 -0300

    update: home page blog section cards and fix testemonials section cards styles

--- src/pages/home/Blog.jsx
+++ src/pages/home/Blog.jsx
@@ -1,18 +1,50 @@
 import { Link } from "react-router-dom";
 import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 import { IoMdArrowForward } from "react-icons/io";
+import { MdArrowOutward } from "react-icons/md";
 
 const Blog = () => {
-
   return (
     <section id="blog" className="blog">
       <div className="wrapper blog__wrapper">
         <h2>TALKATIVE BLOG</h2>
-        <div className="blog__grid-wrapper">
-          <img src={Cat} alt="" className="blog__image" />
-          <img src={Cat} alt="" className="blog__image" />
-          <img src={Cat} alt="" className="blog__image" />
-          <img src={Cat} alt="" className="blog__image" />
+        <div className="blog__cards-wrapper">
+          <div className="blog__card">
+            <img src={Cat} alt="" className="blog__image" />
+            <span>01.01.2024</span>
+            <div className="blog__card-title">
+              <Link to="blog">
+                <span>Lorem ipsum dolor sit amet consectetur.</span>
+              </Link>
+              <Link to="blog" className="button">
+                <MdArrowOutward />
+              </Link>
+            </div>
+          </div>
+          <div className="blog__card">
+            <img src={Cat} alt="" className="blog__image" />
+            <span>01.01.2024</span>
+            <div className="blog__card-title">
+              <Link to="blog">
+                <span>Lorem ipsum dolor sit amet consectetur.</span>
+              </Link>
+              <Link to="blog" className="button">
+                <MdArrowOutward />
+              </Link>
+            </div>
+          </div>
+          <div className="blog__card">
+            <img src={Cat} alt="" className="blog__image" />
+            <span>01.01.2024</span>
+            <div className="blog__card-title">
+              <Link to="blog">
+                <span>Lorem ipsum dolor sit amet consectetur.</span>
+              </Link>
+              <Link to="blog" className="button">
+                <MdArrowOutward />
+              </Link>
+            </div>
+          </div>
         </div>
         <Link to="blog" className="button link">
           Leia mais artigos
