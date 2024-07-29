bbf4fc0cd222976d80328bd9ba56a4c970da24dc
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-07-28 23:45:43 -0300

    feature: add new card for cta section and update cta styles

--- src/pages/home/Cta.jsx
+++ src/pages/home/Cta.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { IoMdArrowForward } from "react-icons/io";
-import { MdHideImage } from "react-icons/md";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 
 const Cta = () => {
   return (
@@ -13,55 +13,29 @@ const Cta = () => {
             Nossos cursos são adaptados às suas necessidades, ajudando você a
             atingir seus objetivos pessoais e profissionais.
           </p>
+          <Link to="contact" className="button cta__button link">
+            Entre em contato
+            <IoMdArrowForward />
+          </Link>
         </div>
         <div className="cta__card-wrapper">
           <div className="cta__card">
-            <MdHideImage />
-            <span>Título</span>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
-              eveniet voluptatibus.
-            </span>
-            <Link to="contact" className="button cta__button link">
-              Entre em contato
-              <IoMdArrowForward />
-            </Link>
-          </div>
-          <div className="cta__card">
-            <MdHideImage />
-            <span>Título</span>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
-              eveniet voluptatibus.
-            </span>
-            <Link to="about" className="button cta__button link">
-              Saiba mais sobre nós
-              <IoMdArrowForward />
-            </Link>
-          </div>
-          <div className="cta__card">
-            <MdHideImage />
-            <span>Título</span>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
-              eveniet voluptatibus.
-            </span>
-            <Link to="expertise" className="button cta__button link">
-              Mais informações
-              <IoMdArrowForward />
-            </Link>
-          </div>
-          <div className="cta__card">
-            <MdHideImage />
-            <span>Título</span>
-            <span>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo cum
-              eveniet voluptatibus.
-            </span>
-            <a href="#faq" className="button cta__button link">
-              Leia nossas FAQs
-              <IoMdArrowForward />
-            </a>
+            <img src={Cat} alt="" />
+            <div>
+              <h3>
+                Talkative <span>Kids</span>
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
+                veritatis. Omnis quibusdam iusto nemo fuga iure, velit quae
+                assumenda quod incidunt rem similique pariatur dignissimos sint
+                eligendi voluptate aut cum!
+              </p>
+              <Link to="expertise" className="button cta__button link">
+                Conheça a Talkative Kids
+                <IoMdArrowForward />
+              </Link>
+            </div>
           </div>
         </div>
       </div>
