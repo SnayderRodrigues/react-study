fa03a5e14d389e5d11a6c33bc15c9f6ee89f7e43
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-03 20:03:48 -0300

    update: blog page hero section component styles

--- src/pages/blog/BlogHero.jsx
+++ src/pages/blog/BlogHero.jsx
@@ -1,13 +1,41 @@
-import React from 'react'
+import React from "react";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
+import { IoMdArrowForward } from "react-icons/io";
 
 const BlogHero = () => {
   return (
     <section className="blogPage-hero">
       <div className="wrapper blogPage-hero__wrapper">
         <h1>Talkative Blog</h1>
+        <div className="blogPage-hero__posts-wrapper">
+          <div className="blogPage-hero__post">
+            <img src={Cat} alt="" />
+            <span>Categoria</span>
+            <span>Lorem ipsum dolor sit amet</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum voluptates.
+            </p>
+            <a href="" className="button">
+              Ler artigo <IoMdArrowForward />
+            </a>
+          </div>
+          <div className="blogPage-hero__post">
+            <img src={Cat} alt="" />
+            <span>Categoria</span>
+            <span>Lorem ipsum dolor sit amet consectetur</span>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatum voluptates.
+            </p>
+            <a href="" className="button">
+              Ler artigo <IoMdArrowForward />
+            </a>
+          </div>
+        </div>
       </div>
     </section>
   );
-}
+};
 
-export default BlogHero
+export default BlogHero;
