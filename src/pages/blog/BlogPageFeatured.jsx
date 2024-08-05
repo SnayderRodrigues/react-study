d88faa3336247e05c34724ef293acc853df554e2
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-04 23:21:19 -0300

    refactor: update blog page featured section to use reusable components and map for rendering posts, improving readability and maintainability

--- src/pages/blog/BlogPageFeatured.jsx
+++ src/pages/blog/BlogPageFeatured.jsx
@@ -1,14 +1,66 @@
-import React from 'react'
+import React from "react";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
+import { IoMdArrowForward } from "react-icons/io";
+
+const FeaturedPost = ({ image, category, title, description, link }) => (
+  <div className="blogPage-featured__post">
+    <img src={image} alt={`Imagem de ${title}`} />
+    <span>{category}</span>
+    <span>{title}</span>
+    <p>{description}</p>
+    <a href={link} className="button">
+      Ler artigo <IoMdArrowForward />
+    </a>
+  </div>
+);
 
 const BlogPageFeatured = () => {
+  const featuredPosts = [
+    {
+      image: Cat,
+      category: "Categoria",
+      title: "Lorem ipsum dolor sit amet consectetur",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptates.",
+      link: "#",
+    },
+    {
+      image: Cat,
+      category: "Categoria",
+      title: "Lorem ipsum dolor sit amet consectetur",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptates.",
+      link: "#",
+    },
+    {
+      image: Cat,
+      category: "Categoria",
+      title: "Lorem ipsum dolor sit amet consectetur",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum voluptates.",
+      link: "#",
+    },
+  ];
+
   return (
     <section className="blogPage-featured">
       <div className="wrapper blogPage-featured__wrapper">
         <h2>Artigos em Destaque</h2>
-        
+        <div className="blogPage-featured__posts-wrapper">
+          {featuredPosts.map((post, index) => (
+            <FeaturedPost
+              key={index}
+              image={post.image}
+              category={post.category}
+              title={post.title}
+              description={post.description}
+              link={post.link}
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
-}
+};
 
-export default BlogPageFeatured
+export default BlogPageFeatured;
