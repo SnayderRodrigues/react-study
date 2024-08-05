2a02aa5515b0b295882681161af65a907c8034a0
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-05 19:40:39 -0300

    feature: integrate React Hook Forms and implement validation for the contact page form component. Update form styles

--- src/pages/contact/ContactPageForm.jsx
+++ src/pages/contact/ContactPageForm.jsx
@@ -1,10 +1,20 @@
-import React from "react";
+import { useForm } from "react-hook-form";
 import { FaWhatsapp } from "react-icons/fa";
 import { MdOutlineAlternateEmail } from "react-icons/md";
 import { FaInstagram } from "react-icons/fa6";
 import { FiPhoneCall } from "react-icons/fi";
 
 const ContactPageForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
     <section className="contactPage-form">
       <div className="wrapper contactPage-form__wrapper ">
@@ -24,27 +34,22 @@ const ContactPageForm = () => {
               <label>
                 Nome<span className="highlight">*</span>
               </label>
-              <input type="text" placeholder="Seu nome" aria-required="true" />
+              <input
+                className={errors?.name && "input-error"}
+                type="text"
+                placeholder="Seu nome"
+                {...register("name", { required: true })}
+              />
+              {errors?.name?.type == "required" && (
+                <span>Este campo é obrigatório.</span>
+              )}
             </div>
             <div className="contactPage-form__form-half">
-              <label>
-                Sobrenome
-                <span className="highlight">*</span>
-              </label>
+              <label>Sobrenome</label>
               <input
                 type="text"
                 placeholder="Seu sobrenome"
-                aria-required="true"
-              />
-            </div>
-            <div className="contactPage-form__form-half">
-              <label>
-                Número de telefone<span className="highlight">*</span>
-              </label>
-              <input
-                type="number"
-                placeholder="Seu número de telefone"
-                aria-required="true"
+                {...register("surname")}
               />
             </div>
             <div className="contactPage-form__form-half">
@@ -52,12 +57,38 @@ const ContactPageForm = () => {
                 Endereço de e-mail<span className="highlight">*</span>
               </label>
               <input
+                className={errors?.email && "input-error"}
                 type="email"
                 placeholder="Seu endereço de e-mail"
-                aria-required="true"
+                {...register("email", { required: true })}
               />
+              {errors?.email?.type == "required" && (
+                <span>Este campo é obrigatório.</span>
+              )}
             </div>
-            <button className="button link">Comece sua jornada</button>
+            <div className="contactPage-form__form-half">
+              <label>
+                Número de telefone<span className="highlight">*</span>
+              </label>
+              <input
+                className={errors?.phone && "input-error"}
+                type="number"
+                placeholder="Seu número de telefone"
+                {...register("phone", { required: true, minLength: 11 })}
+              />
+              {errors?.phone?.type == "minLength" && (
+                <span>Número incompleto.</span>
+              )}
+              {errors?.phone?.type == "required" && (
+                <span>Este campo é obrigatório.</span>
+              )}
+            </div>
+            <button
+              onClick={() => handleSubmit(onSubmit)()}
+              className="button link"
+            >
+              Comece sua jornada
+            </button>
           </div>
           <div className="contactPage-form__contact">
             <div>
