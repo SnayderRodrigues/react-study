11d991afaec3332d1aa49038fe65f361c54df793
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-07 00:00:49 -0300

    refactor: created a reusable course component to avoid code repetition. Added prop-types for property validation

--- src/pages/courses/CoursesPageContent.jsx
+++ src/pages/courses/CoursesPageContent.jsx
@@ -1,95 +1,79 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 
+const Course = ({ title, text, imageFirst }) => (
+  <div className="coursesPage-content__content">
+    {imageFirst ? (
+      <>
+        <div className="coursesPage-content__image-wrapper">
+          <img src={Cat} alt={title} />
+        </div>
+        <div className="coursesPage-content__text-wrapper">
+          <h3>{title}</h3>
+          <p>{text}</p>
+        </div>
+      </>
+    ) : (
+      <>
+        <div className="coursesPage-content__text-wrapper">
+          <h3>{title}</h3>
+          <p>{text}</p>
+        </div>
+        <div className="coursesPage-content__image-wrapper">
+          <img src={Cat} alt={title} />
+        </div>
+      </>
+    )}
+  </div>
+);
+
+Course.propTypes = {
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  imageFirst: PropTypes.bool,
+};
+
+Course.defaultProps = {
+  imageFirst: true,
+};
+
 const CoursesPageContent = () => {
+  const courses = [
+    {
+      title: "Título do Curso 1",
+      text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error, velit delectus! Dolorem esse aut pariatur debitis distinctio, ipsam quidem in et est repudiandae, animi culpa deleniti itaque provident perspiciatis perferendis.",
+      imageFirst: true,
+    },
+    {
+      title: "Título do Curso 2",
+      text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellendus, nam sunt consequuntur exercitationem quos consectetur! Debitis voluptate architecto unde culpa sequi odit doloremque ipsam velit incidunt earum fuga, dignissimos assumenda!",
+      imageFirst: false,
+    },
+    {
+      title: "Título do Curso 3",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed enim, debitis iure dignissimos expedita obcaecati iusto quidem soluta? Dicta nam nisi ea nobis natus expedita consequuntur quas dolor assumenda saepe.",
+      imageFirst: true,
+    },
+    {
+      title: "Título do Curso 4",
+      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo quaerat quis officiis ipsam necessitatibus. Vitae porro quibusdam cupiditate voluptatem asperiores saepe quae accusantium maiores, delectus accusamus nisi corporis ullam molestias.",
+      imageFirst: false,
+    },
+  ];
+
   return (
     <section className="coursesPage-content" id="coursesPageContent">
       <div className="wrapper coursesPage-content__wrapper">
         <h2>Nossos Cursos</h2>
-        <div className="coursesPage-content__content">
-          <div className="coursesPage-content__image-wrapper">
-            <img src={Cat} alt="" />
-          </div>
-          <div className="coursesPage-content__text-wrapper">
-            <h3>Título do Curso</h3>
-            <p>
-              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Error,
-              velit delectus! Dolorem esse aut pariatur debitis distinctio,
-              ipsam quidem in et est repudiandae, animi culpa deleniti itaque
-              provident perspiciatis perferendis.
-            </p>
-          </div>
-        </div>
-        <div className="coursesPage-content__content">
-          <div className="coursesPage-content__text-wrapper">
-            <h3>Título do Curso</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-              Repellendus, nam sunt consequuntur exercitationem quos
-              consectetur! Debitis voluptate architecto unde culpa sequi odit
-              doloremque ipsam velit incidunt earum fuga, dignissimos assumenda!
-            </p>
-          </div>
-          <div className="coursesPage-content__image-wrapper">
-            <img src={Cat} alt="" />
-          </div>
-        </div>
-        <div className="coursesPage-content__content">
-          <div className="coursesPage-content__image-wrapper">
-            <img src={Cat} alt="" />
-          </div>
-          <div className="coursesPage-content__text-wrapper">
-            <h3>Título do Curso</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed enim,
-              debitis iure dignissimos expedita obcaecati iusto quidem soluta?
-              Dicta nam nisi ea nobis natus expedita consequuntur quas dolor
-              assumenda saepe.
-            </p>
-          </div>
-        </div>
-        <div className="coursesPage-content__content">
-          <div className="coursesPage-content__text-wrapper">
-            <h3>Título do Curso</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
-              quaerat quis officiis ipsam necessitatibus. Vitae porro quibusdam
-              cupiditate voluptatem asperiores saepe quae accusantium maiores,
-              delectus accusamus nisi corporis ullam molestias.
-            </p>
-          </div>
-          <div className="coursesPage-content__image-wrapper">
-            <img src={Cat} alt="" />
-          </div>
-        </div>
-        <div className="coursesPage-content__content">
-          <div className="coursesPage-content__image-wrapper">
-            <img src={Cat} alt="" />
-          </div>
-          <div className="coursesPage-content__text-wrapper">
-            <h3>Título do Curso</h3>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus
-              iusto voluptatum fugiat eveniet ipsa vel dolore porro distinctio
-              sit! Atque placeat sapiente commodi recusandae et expedita quia,
-              quos quasi mollitia!
-            </p>
-          </div>
-        </div>
-        <div className="coursesPage-content__content">
-          <div className="coursesPage-content__text-wrapper">
-            <h3>Título do Curso</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-              in provident porro! Excepturi amet aliquid vitae. Facilis,
-              consequuntur et saepe quae repellat quod qui natus cupiditate
-              incidunt! Omnis, expedita porro!
-            </p>
-          </div>
-          <div className="coursesPage-content__image-wrapper">
-            <img src={Cat} alt="" />
-          </div>
-        </div>
+        {courses.map((course, index) => (
+          <Course
+            key={index}
+            title={course.title}
+            text={course.text}
+            imageFirst={course.imageFirst}
+          />
+        ))}
       </div>
     </section>
   );
