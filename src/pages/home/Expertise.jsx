abb25139c379997d978ba4727b4895240b339fc2
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-07 17:37:40 -0300

    refactor: homepage expertise section for performance and maintainability. Update expertise section styles

--- src/pages/home/Expertise.jsx
+++ src/pages/home/Expertise.jsx
@@ -2,6 +2,57 @@ import { Link } from "react-router-dom";
 import { IoMdArrowForward } from "react-icons/io";
 import { MdHideImage } from "react-icons/md";
 
+const expertises = [
+  {
+    icon: <MdHideImage />,
+    title: "Aulas ao vivo 24h",
+    description:
+      "Disponibilidade de aulas ao vivo em qualquer horário, permitindo flexibilidade para os alunos.",
+  },
+  {
+    icon: <MdHideImage />,
+    title: "Professores Nativos",
+    description:
+      "Instrutores que são falantes nativos de inglês, garantindo uma experiência autêntica e imersiva.",
+  },
+  {
+    icon: <MdHideImage />,
+    title: "Conteúdo Inovador",
+    description:
+      "Material didático atualizado e inovador que acompanha as tendências mais recentes de ensino.",
+  },
+  {
+    icon: <MdHideImage />,
+    title: "Metodologia Personalizada",
+    description:
+      "Planos de estudo personalizados de acordo com as necessidades e objetivos de cada aluno.",
+  },
+  {
+    icon: <MdHideImage />,
+    title: "Acompanhamento Individualizado",
+    description:
+      "Feedback constante e suporte individual para acompanhar o progresso dos alunos.",
+  },
+  {
+    icon: <MdHideImage />,
+    title: "Plataforma Interativa",
+    description:
+      "Uso de uma plataforma online interativa que facilita o aprendizado e o engajamento dos alunos.",
+  },
+  {
+    icon: <MdHideImage />,
+    title: "Aulas de Conversação",
+    description:
+      "Sessões focadas em melhorar a fluência e a confiança dos alunos ao falar inglês.",
+  },
+  {
+    icon: <MdHideImage />,
+    title: "Preparação para Exames",
+    description:
+      "Cursos específicos para preparação de exames de proficiência, como TOEFL, IELTS, entre outros.",
+  },
+];
+
 const Expertise = () => {
   return (
     <section id="expertise" className="expertise">
@@ -12,75 +63,18 @@ const Expertise = () => {
           características que fazem da nossa escola a escolha ideal:
         </p>
         <div className="expertise__cards-wrapper">
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Aulas ao vivo 24h</span>
-            <p>
-              Disponibilidade de aulas ao vivo em qualquer horário, permitindo
-              flexibilidade para os alunos.
-            </p>
-          </div>
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Professores Nativos</span>
-            <p>
-              Instrutores que são falantes nativos de inglês, garantindo uma
-              experiência autêntica e imersiva.
-            </p>
-          </div>
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Conteúdo Inovador</span>
-            <p>
-              Material didático atualizado e inovador que acompanha as
-              tendências mais recentes de ensino.
-            </p>
-          </div>
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Metodologia Personalizada</span>
-            <p>
-              Planos de estudo personalizados de acordo com as necessidades e
-              objetivos de cada aluno.
-            </p>
-          </div>
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Acompanhamento Individualizado</span>
-            <p>
-              Feedback constante e suporte individual para acompanhar o
-              progresso dos alunos.
-            </p>
-          </div>
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Plataforma Interativa</span>
-            <p>
-              Uso de uma plataforma online interativa que facilita o aprendizado
-              e o engajamento dos alunos.
-            </p>
-          </div>
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Aulas de Conversação</span>
-            <p>
-              Sessões focadas em melhorar a fluência e a confiança dos alunos ao
-              falar inglês.
-            </p>
-          </div>
-          <div className="expertise__card">
-            <MdHideImage />
-            <span>Preparação para Exames</span>
-            <p>
-              Cursos específicos para preparação de exames de proficiência, como
-              TOEFL, IELTS, entre outros.
-            </p>
-          </div>
-          <Link to="about" className="button link">
-            Mais informações
-            <IoMdArrowForward />
-          </Link>
+          {expertises.map((expertise, index) => (
+            <div key={index} className="expertise__card">
+              {expertise.icon}
+              <span>{expertise.title}</span>
+              <p>{expertise.description}</p>
+            </div>
+          ))}
         </div>
+        <Link to="about" className="button link">
+          Mais informações
+          <IoMdArrowForward />
+        </Link>
       </div>
     </section>
   );
