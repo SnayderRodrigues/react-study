3cc002b471e342dac32b7a10af2d744da2cd4303
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-07 18:22:47 -0300

    update: update homepage seciton paddings and margins, fix about page team component and update home page expertise section styles

--- src/pages/about/AboutPageTeam.jsx
+++ src/pages/about/AboutPageTeam.jsx
@@ -4,7 +4,7 @@ import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 const TeamMemberCard = ({ name, role, description, image }) => {
   return (
     <div className="aboutPage-team__card">
-      <img src={image} alt={name} />
+      <img src={image} alt={name}/>
       <span>{name}</span>
       <span>{role}</span>
       <span>{description}</span>
@@ -15,48 +15,56 @@ const TeamMemberCard = ({ name, role, description, image }) => {
 const AboutPageTeam = () => {
   const teamMembers = [
     {
+      id: "1",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
       image: Cat,
     },
     {
+      id: "2",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
       image: Cat,
     },
     {
+      id: "3",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
       image: Cat,
     },
     {
+      id: "4",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
       image: Cat,
     },
     {
+      id: "5",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
       image: Cat,
     },
     {
+      id: "6",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
       image: Cat,
     },
     {
+      id: "7",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
       image: Cat,
     },
     {
+      id: "8",
       name: "Nome e Sobrenome",
       role: "Cargo ou Função",
       description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
@@ -68,7 +76,7 @@ const AboutPageTeam = () => {
     <section className="aboutPage-team">
       <div className="wrapper aboutPage-team__wrapper">
         {teamMembers.map((member) => (
-          <TeamMemberCard key={member.name} {...member} />
+          <TeamMemberCard key={member.id} {...member} />
         ))}
       </div>
     </section>
