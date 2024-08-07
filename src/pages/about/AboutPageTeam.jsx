775d692796e6085481501e2e08d6a1284651693a
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-07 00:11:17 -0300

    refactor: AboutPageTeam section component using map and spread operator. Added TeamMemberCard component

--- src/pages/about/AboutPageTeam.jsx
+++ src/pages/about/AboutPageTeam.jsx
@@ -1,82 +1,75 @@
 import React from "react";
 import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 
+const TeamMemberCard = ({ name, role, description, image }) => {
+  return (
+    <div className="aboutPage-team__card">
+      <img src={image} alt={name} />
+      <span>{name}</span>
+      <span>{role}</span>
+      <span>{description}</span>
+    </div>
+  );
+};
+
 const AboutPageTeam = () => {
+  const teamMembers = [
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+    {
+      name: "Nome e Sobrenome",
+      role: "Cargo ou Função",
+      description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit.",
+      image: Cat,
+    },
+  ];
+
   return (
     <section className="aboutPage-team">
       <div className="wrapper aboutPage-team__wrapper">
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
-        <div className="aboutPage-team__card">
-          <img src={Cat} alt="" />
-          <span>Nome e Sobrenome</span>
-          <span>Cargo ou Função</span>
-          <span>
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi eum
-            maiores ullam velit.
-          </span>
-        </div>
+        {teamMembers.map((member) => (
+          <TeamMemberCard key={member.name} {...member} />
+        ))}
       </div>
     </section>
   );
