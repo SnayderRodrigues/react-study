f05080615929b65cd3cfbf0a25e81e567a75cf1d
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-09 23:44:05 -0300

    update: AboutPageTestimonials section component styles

--- src/pages/about/AboutPageTestimonials.jsx
+++ src/pages/about/AboutPageTestimonials.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 import { IoMdArrowForward } from "react-icons/io";
-import { FaAsterisk } from "react-icons/fa";
 
 const AboutPageTestimonials = () => {
   const testimonialsItems = [
@@ -10,24 +9,28 @@ const AboutPageTestimonials = () => {
       testimonial:
         "A Talkative transformou minha experiência de aprendizado. As aulas são dinâmicas e os professores são extremamente dedicados. Hoje, me sinto muito mais confiante para conversar em inglês!",
       author: "Ana Paula",
+      status: "Aluna, 07 meses",
     },
     {
       number: "02.",
       testimonial:
         "A flexibilidade de horários e a qualidade dos professores da Talkative são incríveis. Consigo estudar no meu próprio ritmo, o que tem sido essencial para o meu desenvolvimento. Melhor decisão que já tomei!",
       author: "Lucas",
+      status: "Aluno, 06 meses",
     },
     {
       number: "03.",
       testimonial:
         "A metodologia da Talkative é inovadora e envolvente. Sinto que meu inglês evoluiu muito mais rápido do que em outros cursos que já fiz. As aulas online são práticas e adaptadas às minhas necessidades. Estou muito satisfeito com os resultados!",
       author: "Ricardo",
+      status: "Aluno, formado",
     },
     {
       number: "04.",
       testimonial:
         "Graças à Talkative, consegui uma promoção no trabalho que exigia fluência em inglês. As aulas são práticas e focadas em situações reais do dia a dia. Estou muito satisfeita com meu progresso!",
       author: "Mariana",
+      status: "Aluna, formada",
     },
   ];
 
@@ -53,29 +56,6 @@ const AboutPageTestimonials = () => {
     <section className="aboutPage-testimonials">
       <div className="wrapper aboutPage-testimonials__wrapper">
         <h2>Depoimentos de Nossos Alunos</h2>
-        {testimonialsItems.map((item, index) => (
-          <div
-            className={
-              index === currentIndex
-                ? "aboutPage-testimonials__card card-active"
-                : "aboutPage-testimonials__card"
-            }
-            key={index}
-          >
-            <div className="aboutPage-testimonials__card-content">
-              <span>{item.number}</span>
-              <div>
-                <div className="aboutPage-testimonials__headline">
-                  <span className="aboutPage-testimonials__author">
-                    <FaAsterisk /> {item.author}
-                  </span>
-                  <p>"{item.testimonial}"</p>
-                </div>
-                <img src={Cat} alt="" />
-              </div>
-            </div>
-          </div>
-        ))}
         <div className="navigation">
           <div className="navigation-div">
             <button
@@ -93,6 +73,32 @@ const AboutPageTestimonials = () => {
               <IoMdArrowForward />
             </button>
           </div>
+        </div>
+        <div className="aboutPage-testimonials__card-wrapper">
+          {testimonialsItems.map((item, index) => (
+            <div
+              className={
+                index === currentIndex
+                  ? "aboutPage-testimonials__card card-active"
+                  : "aboutPage-testimonials__card"
+              }
+              key={index}
+            >
+              <div className="aboutPage-testimonials__card-header">
+                <span>{item.number}</span>
+                <img src={Cat} alt="" />
+              </div>
+              <span className="aboutPage-testimonials__card-author">
+                {item.author}
+              </span>
+              <div className="aboutPage-testimonials__card-description">
+                <div>
+                  <span>{item.status}</span>
+                  <p>"{item.testimonial}"</p>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
