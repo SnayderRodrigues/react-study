0d092a8471b72baafacc360df41277162e00a3bd
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-08-12 13:29:33 -0300

    update: redesign of the page footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,77 +1,88 @@
 import { Link } from "react-router-dom";
-import { FaLinkedinIn } from "react-icons/fa";
-import { FaInstagram } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-import { FaFacebook } from "react-icons/fa";
-import { FaAsterisk } from "react-icons/fa";
 import { IoMdArrowForward } from "react-icons/io";
 
 const Footer = () => {
-
-  // const contactLink = (e) => e.target.textContent = "Let's create something extraordinary!";
-
   return (
     <footer id="footer" className="footer">
-      <div className="wrapper contact__wrapper">
-        <div className="contact__headline">
-          <span>Abra portas para o seu futuro:</span>
-          <Link to="contact" className="footer__contact-link">
-            <span>
-              <span className="link">
-                Junte-se a nós hoje <IoMdArrowForward />
-              </span>
-              e comece sua jornada.
+      <div className="wrapper footer__wrapper">
+        <Link to="contact" className="footer__headline">
+          <span>
+            <span className="link">
+              Matricule-se hoje <IoMdArrowForward />
             </span>
-          </Link>
-        </div>
-        <div className="contact__information">
+            e comece sua jornada
+          </span>
+        </Link>
+        <div className="footer__link-columns">
+          <a href="/" className="footer__logo">
+            Talkative
+          </a>
           <div>
-            <span>
-              <FaAsterisk />
-              Contato
-            </span>
-            <span>(00) 00000-0000</span>
-            <span>contato@talkative.com</span>
+            <div className="footer__navigation-wrapper">
+              <span>Talkative</span>
+              <ul className="footer__navigation-ul">
+                <li>
+                  <Link to="/" className="link">
+                    Início
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/about" className="link">
+                    Sobre
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/courses" className="link">
+                    Cursos
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/contact" className="link">
+                    Contato
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer__socials-wrapper">
+              <span>Sociais</span>
+              <ul className="footer__socials-ul">
+                <li>
+                  <Link to="/" className="link">
+                    Facebook
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="link">
+                    Instagram
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="link">
+                    LinkedIn
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/" className="link">
+                    X
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="footer__contact-wrapper">
+              <span>Contato</span>
+              <Link to="/">(00) 00000-0000</Link>
+              <Link to="/">contato@talkative.com</Link>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="wrapper footer__wrapper">
-        <div className="footer__logo-wrapper">
-          <a href="/" className="footer__logo">
-            <span>Talkative</span>
-          </a>
-          <span>
-            &copy; {new Date().getFullYear()} Talkative. Todos os direitos
-            reservados.
-          </span>
-        </div>
-        <div className="footer__socials-wrapper">
-          <span>
-            <FaAsterisk />
-            Redes Sociais
-          </span>
-          <ul className="footer__socials-ul">
-            <li>
-              <a href="" className="link button">
-                <FaFacebook /> Facebook
-              </a>
-            </li>
-            <li>
-              <a href="" className="link button">
-                <FaInstagram /> Instagram
-              </a>
-            </li>
-            <li>
-              <a href="" className="link button">
-                <FaLinkedinIn /> LinkedIn
-              </a>
-            </li>
-            <li>
-              <a href="" className="link button">
-                <FaXTwitter /> X
-              </a>
-            </li>
-          </ul>
+        <div className="footer__legal-wrapper">
+          <span>© Talkative, 2024.</span>
+          <Link to="/" className="link">
+            Termos de uso <IoMdArrowForward />
+          </Link>
+          <Link to="/" className="link">
+            Políticas de privacidade <IoMdArrowForward />
+          </Link>
         </div>
       </div>
     </footer>
