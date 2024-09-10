8b18af1089561836e32ce06573a2cb19f135980b
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-09-09 23:44:16 -0300

    update: HomePage blog section styles, BlogPageHero styles and update BlogArticlePage structure

--- src/pages/blog/BlogArticlePage.jsx
+++ src/pages/blog/BlogArticlePage.jsx
@@ -8,7 +8,9 @@ const articles = [
     title: "5 Dicas Essenciais para Melhorar Sua Pronúncia em Inglês",
     content: "Conteúdo do artigo sobre pronúncia.",
     image: Cat,
+    author: "Nome do Autor",
     date: "01/01/2024",
+    readTime: "05 min de leitura",
     category: "Categoria",
   },
   {
@@ -16,7 +18,9 @@ const articles = [
     title: "Por Que Aprender Inglês Desde a Infância Faz Toda a Diferença?",
     content: "Conteúdo do artigo sobre aprender inglês na infância.",
     image: Cat,
+    author: "Nome do Autor",
     date: "01/01/2024",
+    readTime: "05 min de leitura",
     category: "Categoria",
   },
   {
@@ -25,7 +29,9 @@ const articles = [
       "Inglês para Adolescentes: Como Conectar o Ensino com o Mundo Digital",
     content: "Conteúdo do artigo sobre adolescentes e o ensino digital.",
     image: Cat,
+    author: "Nome do Autor",
     date: "01/01/2024",
+    readTime: "05 min de leitura",
     category: "Categoria",
   },
 ];
@@ -39,13 +45,17 @@ const BlogArticlePage = () => {
   }
 
   return (
-    <section className="wrapper">
-      <img src={article.image} alt={article.title} />
-      <h1>{article.title}</h1>
-      <p>{article.content}</p>
-      <span>{article.category}</span>
-      <span>{article.date}</span>
-      <Link to="/blog">Voltar</Link>
+    <section className="blogArticlePage">
+      <div className="wrapper blogArticlePage__wrapper">
+        <Link to="/blog">Voltar</Link>
+        <span>{article.category}</span>
+        <h1>{article.title}</h1>
+        <span>{article.author}</span>
+        <span>{article.date}</span>
+        <span>{article.readTime}</span>
+        <img src={article.image} alt={article.title} />
+        <p>{article.content}</p>
+      </div>
     </section>
   );
 };
--- src/pages/home/Blog.jsx
+++ src/pages/home/Blog.jsx
@@ -3,23 +3,23 @@ import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 import { IoMdArrowForward } from "react-icons/io";
 import { MdArrowOutward } from "react-icons/md";
 
-const BlogCard = ({ image, category, date, title, link }) => (
-  <div className="blog__card">
+const BlogPost = ({ image, category, readTime, date, title, slug }) => (
+  <div className="blog__post">
     <img
       src={image}
-      alt={`${title}`}
-      className="blog__image"
+      alt={`Imagem de ${title}`}
+      className="blog__post-image"
     />
-    <div>
+    <div className="blog__post-text">
       <span>{category}</span>
-      <span>{date}</span>
-    </div>
-    <div className="blog__card-title">
-      <Link to={link}>
-        <span>{title}</span>
-      </Link>
-      <Link to={link} className="button">
-        <MdArrowOutward />
+      <span>{title}</span>
+      <div>
+        <span>{date}</span>
+        <span>•</span>
+        <span>{readTime}</span>
+      </div>
+      <Link to={`/blog/${slug}`} className="button">
+        Ler artigo <IoMdArrowForward />
       </Link>
     </div>
   </div>
@@ -30,47 +30,53 @@ const Blog = () => {
     {
       image: Cat,
       category: "Categoria",
-      date: "01.01.2024",
+      date: "01 Jan.2024",
+      readTime: "05 min de leitura",
       title: "5 Dicas Essenciais para Melhorar Sua Pronúncia em Inglês",
-      link: "blog",
+      slug: "5-dicas-para-melhorar-pronuncia",
     },
     {
       image: Cat,
       category: "Categoria",
-      date: "01.01.2024",
+      date: "01 Jan.2024",
+      readTime: "05 min de leitura",
       title: "Por Que Aprender Inglês Desde a Infância Faz Toda a Diferença?",
-      link: "blog",
+      slug: "aprender-ingles-desde-infancia",
     },
     {
       image: Cat,
       category: "Categoria",
-      date: "01.01.2024",
+      date: "01 Jan.2024",
+      readTime: "05 min de leitura",
       title:
         "Inglês para Adolescentes: Como Conectar o Ensino com o Mundo Digital",
-      link: "blog",
+      slug: "ingles-para-adolescentes",
     },
   ];
 
   return (
     <section id="blog" className="blog">
       <div className="wrapper blog__wrapper">
-        <h2>Talkative Blog</h2>
-        <div className="blog__cards-wrapper">
+        <div className="blog__headline-wrapper">
+          <h2>Talkative Blog</h2>
+          <Link to="blog" className="button link">
+            Ver tudo
+            <IoMdArrowForward />
+          </Link>
+        </div>
+        <div className="blog__posts-wrapper">
           {blogPosts.map((post, index) => (
-            <BlogCard
+            <BlogPost
               key={index}
               image={post.image}
               category={post.category}
               date={post.date}
+              readTime={post.readTime}
               title={post.title}
-              link={post.link}
+              slug={post.slug}
             />
           ))}
         </div>
-        <Link to="blog" className="button link">
-          Leia mais artigos
-          <IoMdArrowForward />
-        </Link>
       </div>
     </section>
   );
