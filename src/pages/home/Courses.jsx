82c65fef6ceb81ba410c7a5be56eacb6bc559ebf
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-09-13 23:53:49 -0300

    update: HomePage Courses section now redirect to individual courses

--- src/pages/home/Courses.jsx
+++ src/pages/home/Courses.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import { IoMdArrowForward } from "react-icons/io";
 import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
 
-const CourseCard = ({ title, subtitle, description, link }) => (
+const CourseCard = ({ title, subtitle, description, slug }) => (
   <div className="courses__card">
     <img src={Cat} alt={`${title} ${subtitle}`} />
     <div>
@@ -11,8 +11,8 @@ const CourseCard = ({ title, subtitle, description, link }) => (
         {title} <span>{subtitle}</span>
       </h3>
       <p>{description}</p>
-      <Link to={link} className="button courses__button">
-        Conheça a {title} {subtitle}
+      <Link to={`/courses/${slug}`} className="button courses__button">
+        Saiba mais
         <IoMdArrowForward />
       </Link>
     </div>
@@ -24,30 +24,30 @@ const Courses = () => {
     {
       title: "Talkative",
       subtitle: "Complete",
-      link: "courses",
       description:
         "O curso ideal para quem deseja aprender inglês de forma completa, desde o básico até o avançado. Aborda todas as habilidades linguísticas – fala, escuta, leitura e escrita – com foco na comunicação eficaz e fluente.",
+      slug: "talkative-complete",
     },
     {
       title: "Talkative",
       subtitle: "Kids",
-      link: "courses",
       description:
         "Um curso lúdico e interativo especialmente desenvolvido para crianças. Com atividades envolventes, as crianças aprendem inglês de maneira natural e divertida, despertando o interesse pelo idioma desde cedo.",
+      slug: "talkative-kids",
     },
     {
       title: "Talkative",
       subtitle: "Teens",
-      link: "courses",
       description:
         "Voltado para adolescentes, este curso equilibra a necessidade de se comunicar em inglês com temas e conteúdos que refletem os interesses e desafios da adolescência. Ideal para quem quer se preparar para o futuro.",
+      slug: "talkative-teens",
     },
     {
       title: "Talkative",
       subtitle: "Express",
-      link: "courses",
       description:
         "Um curso intensivo para quem precisa aprender inglês de forma rápida e eficiente. Focado em resultados a curto prazo, com aulas dinâmicas que priorizam a prática e a comunicação em situações reais.",
+      slug: "talkative-express",
     },
   ];
 
@@ -62,9 +62,21 @@ const Courses = () => {
               title={course.title}
               subtitle={course.subtitle}
               description={course.description}
-              link={course.link}
+              slug={course.slug}
             />
           ))}
+          <div className="courses__card">
+            <h3>Conheça todos os cursos.</h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+              deleniti molestiae distinctio delectus fuga, beatae sit rerum
+              quisquam doloribus.
+            </p>
+            <Link to="/courses" className="button courses__button">
+              Saiba mais
+              <IoMdArrowForward />
+            </Link>
+          </div>
         </div>
       </div>
     </section>
