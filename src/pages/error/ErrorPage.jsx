10bb48f9b95c925ebdb7352c2ad1fa5312bc9b84
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-09-18 18:54:44 -0300

    feat: update ErrorPage component. Add style files and folder structure

--- src/pages/error/ErrorPage.jsx
+++ src/pages/error/ErrorPage.jsx
@@ -1,10 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+import Cat from "../../img/IMG_20200407_163224926_HDR-01.jpeg";
+import { IoMdArrowBack } from "react-icons/io";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>ErrorPage</h1>
-    </div>
+    <>
+      <Header />
+      <section className="errorPage">
+        <div className="wrapper errorPage__wrapper">
+          <h2 className="errorPage__headline">
+            Ops! <br />
+            Página não encontrada.
+          </h2>
+          <Link to="/" className="button errorPage__button">
+            <IoMdArrowBack />
+            Retornar à página inicial
+          </Link>
+          <img src={Cat} alt="" className="errorPage__image" />
+        </div>
+      </section>
+      <Footer />
+    </>
   );
 };
 
