c341837e5de22bf1eac5bfb49cf07cc9a2255c1d
Snayder Rodrigues <snayderrodrigues@gmail.com>
2024-09-30 23:57:58 -0300

    feat: add window resize listener to handle navbar transition

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,27 +4,16 @@ import { Link, useLocation } from "react-router-dom";
 const Header = () => {
   const location = useLocation();
   const headerRef = useRef(null);
+  const navbarRef = useRef(null);
   const [openMenu, setOpenMenu] = useState(false);
 
-  const isHomePage = () => {
-    return location.pathname === "/";
-  };
-  const isAboutPage = () => {
-    return location.pathname === "/about";
-  };
-  const isCoursesPage = () => {
-    return location.pathname.startsWith("/courses");
-  };
-  const isBlogPage = () => {
-    return location.pathname.startsWith("/blog");
-  };
-  const isContactPage = () => {
-    return location.pathname === "/contact";
-  };
+  const isHomePage = () => location.pathname === "/";
+  const isAboutPage = () => location.pathname === "/about";
+  const isCoursesPage = () => location.pathname.startsWith("/courses");
+  const isBlogPage = () => location.pathname.startsWith("/blog");
+  const isContactPage = () => location.pathname === "/contact";
 
-  const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  };
+  const scrollToTop = () => window.scrollTo({ top: 0, behavior: "smooth" });
 
   const handleScroll = () => {
     const header = headerRef.current;
@@ -42,10 +31,25 @@ const Header = () => {
     };
   }, []);
 
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 768) {
+        if (navbarRef.current) {
+          navbarRef.current.style.transition = "none";
+        }
+      }
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const handleOpenMenu = () => {
     setOpenMenu((prevState) => !prevState);
   };
-
 
   return (
     <header ref={headerRef} id="header" className="header">
@@ -54,7 +58,10 @@ const Header = () => {
           <a href="/" className="header__logo" onClick={scrollToTop}>
             <span>Talkative</span>
           </a>
-          <ul className={`header__navbar-ul ${openMenu ? "open" : ""}`}>
+          <ul
+            ref={navbarRef}
+            className={`header__navbar-ul ${openMenu ? "open" : ""}`}
+          >
             <li className="header__navbar-li">
               <Link
                 to="/"
